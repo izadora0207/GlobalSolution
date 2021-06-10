bdbae8ba03ee82eb6b369c5b646e951212b2b5c6
DiegoSousaRodrigues2 <rm87910@fiap.com.br>
2021-06-10 00:13:40 -0300

    mec dms pode falar

--- testes_de_JS/teste.js
+++ testes_de_JS/teste.js
@@ -1,9 +1,34 @@
-function alteraNota(){
-    var radios = document.getElementsByName("fb");
+var artigos = ["loja","academia"]
+var Nota = [0,0];
+var atualizaNota;
 
-    for (var i = 0; i < radios.length; i++) {
+
+function alteraNota(nome, posicao){
+    let radios = document.getElementsByName(nome);
+    p = posicao;
+
+    for (let i = 0; i < radios.length; i++) {
         if (radios[i].checked) {
             alert("Escolheu: " + radios[i].value);
+            r = parseInt(radios[i].value);
+
+            atualizarNota(p, r);
         }
     }
+    
+    /*
+    splice(adiciona ,apaga , Add->esse valor)
+    */ 
+}
+
+function atualizarNota(posicao, rr){
+    p = posicao;
+    r = rr;
+    atualizaNota = Nota[p] + r;
+    Nota.splice(p, p+1, atualizaNota);
+    exibirNota();
+}
+
+function exibirNota(){ 
+    alert("no artigo da " + artigos[p] + " a media da nota foi " + Nota[p]);     
 }
